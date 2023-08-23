652a0252ba6718c715b56eaadca3799b36d98000
hyperion-astra <eugeneemovon@gmail.com>
2023-08-23 12:48:40 +0100

    ff

--- src/App.js
+++ src/App.js
@@ -36,8 +36,13 @@ function App() {
             <Route exact path="/" element={<Home />} />
             {/* <Route path="/register" element={<Register />} /> */}
             <Route path="/account" element={<Account />} />
-            <Route path='/profile/:id' element={<Profile />}/>
-            <Route path='/user/*' element={<User />} />
+            {/* <Route path='/profile/:id' element={<Profile />}/> */}
+            {/* <Route path='/user/*' element={<User />} /> */}
+            <Route path="/user/*" element={
+                <ProtectedRoute isLoggedIn={true}>   {/*the flag isLoggedIn is simulating a logged in account, if not, when clicked, it will go to the homepage*/}
+                    <User />
+                </ProtectedRoute>
+            } />
         </Routes>
       </main>
     </div>
